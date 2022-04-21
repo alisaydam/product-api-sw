45680da92f8a4ffa69576515cb764cae3ac54d81
KereBere <alsydm091@gmail.com>
2022-04-21 17:13:33 +0300

    db name added

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 define("DB_HOST", "us-cdbr-east-05.cleardb.net");
 define("DB_USERNAME", "ba871319ce1487");
 define("DB_PASSWORD", "7dbde77c");
-define("DB_NAME", "product_api");
+define("DB_NAME", "heroku_b023fc8462d3a8e");
 
 
 
