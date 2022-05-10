5a0c7b1422fb826900aa360b5c2e615655a92795
KereBere <alsydm091@gmail.com>
2022-05-10 10:06:53 +0300

    Delete cors fixed

--- api/product/delete.php
+++ api/product/delete.php
@@ -1,9 +1,7 @@
 <?php
-// Headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT,DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
 include_once '../../config/Database.php';
 include_once '../../models/Product.php';
