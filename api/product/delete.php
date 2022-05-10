779a1d2cb93f8d298a80dc5de29fdfe375124d17
KereBere <alsydm091@gmail.com>
2022-05-10 10:10:07 +0300

    Delete Cors fixex-2

--- api/product/delete.php
+++ api/product/delete.php
@@ -1,7 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT,DELETE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 include_once '../../config/Database.php';
 include_once '../../models/Product.php';
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST, DELETE');
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: appication/json");
